0ffb6aafacfe0dc7fa8e4002826db45ed5d3ff1b
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2021-04-21 00:15:52 +0800

    1. holeCardsToChar() 2. playerCombos() 3. montecarlo()

--- cpp_omp/mc.cpp
+++ cpp_omp/mc.cpp
@@ -8,6 +8,16 @@
 #include <omp/CardRange.h>
 #include <omp/EquityCalculator.h>
 namespace py = pybind11;
+
+
+/*==============================================================================================*\
+    FUNCTION :   holeCardsToChar()
+    PURPOSE  :   Convert rank,suit of single card into string.
+
+    ARGUMENT :   rank : 0~12 represents duece,3,...K,A
+                 suit  :0~3 represents spade, heart, cubic, diamond
+    RETURN   :   Return string expression of card.
+\*==============================================================================================*/
 std::string holeCardsToChar(unsigned rank, unsigned suit) {
 	std::string s;
     char tmp,tmp1;
@@ -55,117 +65,56 @@ std::string holeCardsToChar(unsigned rank, unsigned suit) {
     return s;
 }
 
-std::string montecarlo(const std::vector<std::string>& ranges, std::string board, std::string dead)
+/*==============================================================================================*\
+    FUNCTION :   montecarlo()
+    PURPOSE  :   Calculate equity for each player
+
+    ARGUMENT :   ranges : list of ranges for active players.
+                 board  :  board card
+                 dead   :  dead card dealt alreay.
+    RETURN   :   Return equity for each player.
+\*==============================================================================================*/
+std::vector<double> montecarlo(const std::vector<std::string>& ranges, std::string board, std::string dead)
 { 
     std::vector<omp::CardRange> ranges2(ranges.begin(), ranges.end());
-//    std::vector<omp::CardRange> ranges2;
-//    for( std::string const & c : ranges ){
-//        ranges2.push_back(omp::CardRange(c));
-//    }
     omp::EquityCalculator eq;
     eq.start(ranges2, omp::CardRange::getCardMask(board), omp::CardRange::getCardMask(dead), true);
     eq.wait();
     auto results = eq.getResults();
-    std::string result;
-    // Returns a list of card combinations belonging to this range. Guarantees that there are no duplicates.
-    // Cards in each combo are ordered so that the bigger rank is always first. The whole vector is sorted in the
-    // following order: 1) rank of first card 2) rank of second card 3) suit of first card 4) suit of second card
-    // Create a Hand from a card. CardIdx is an integer between 0 and 51, so that CARD = 4 * RANK + SUIT, where
-    // rank ranges from 0 (deuce) to 12 (ace) and suit is from 0 (spade) to 3 (diamond).
-    std::cout << "----------- Original Range -----------" << std::endl;
-    for (std::string const& c : ranges) {
-        std::cout << c << std::endl;
-        std::vector<std::array<uint8_t,2>> temp = omp::CardRange(c).combinations();
-        size_t size1 = temp.size();
-        for (int j=0; j < size1; j++){
-            unsigned r1, r2, s1, s2;
-            r1 = temp[j][0] >> 2 ;
-            s1 = temp[j][0] &  3;
-            r2 = temp[j][1] >> 2 ;
-            s2 = temp[j][1] &  3;
-            std::cout << " " << holeCardsToChar(r1, s1) << holeCardsToChar(r2, s2) ;
-            result.push_back(holeCardsToChar(r1, s1)[0]);
-            result.push_back(holeCardsToChar(r1, s1)[1]);
-            result.push_back(holeCardsToChar(r2, s2)[0]);
-            result.push_back(holeCardsToChar(r2, s2)[1]);
-            result.append(",");
-        }
-        std::cout << std::endl;
-        std::cout << "size of range: "<< size1 << std::endl;
+    size_t mPlayerCount = ranges2.size();
+    std::vector<double> mResults;
+
+    for(unsigned i = 0 ; i<mPlayerCount; ++i) {
+        mResults.push_back(results.equity[i]);
     }
+    std::cout << results.equity[0] << " " << results.equity[1] <<" "<< results.equity[2] << std::endl;
+    std::cout << results.equity[3] << " " << results.equity[4] << " " << results.equity[5] << std::endl;
 
-    // Set up card ranges.
-    unsigned mCombinedRangeCount;
-    uint64_t mDeadCards, mBoardCards;
-    std::vector<omp::CardRange> mOriginalHandRanges;
-    std::vector<std::vector<std::array<uint8_t,2>>> mHandRanges; // Ranges after card removal.
-    static const size_t MAX_COMBINED_RANGE_SIZE = 10000;
-
-    mDeadCards = omp::CardRange::getCardMask(dead);
-    mBoardCards = omp::CardRange::getCardMask(board);
-    mOriginalHandRanges = ranges2;
-    mHandRanges = omp::EquityCalculator::removeInvalidCombos(ranges2, mDeadCards | mBoardCards);
-    std::vector<omp::CombinedRange> combinedRanges = omp::CombinedRange::joinRanges(mHandRanges, MAX_COMBINED_RANGE_SIZE);
-//    for (unsigned i = 0; i < combinedRanges.size(); ++i) {
-//        if (combinedRanges[i].combos().size() == 0)
-//            //std::string temp12 = "No Combos";
-//            std::vector<std::string> result_temp = {"No Combos"};
-//            //result_temp.push_back(temp12);
-//            return result_temp;
-//    }
-    mCombinedRangeCount = (unsigned)combinedRanges.size();
-
-    std::cout << "-----------Combined Range -----------" << std::endl;
-    for(std::vector<std::array<uint8_t,2>> const& c : mHandRanges){
-        size_t size2 = c.size();
-        for( unsigned i=0; i<size2; i++){
-            unsigned r1, r2, s1, s2;
-            r1 = c[i][0] >> 2 ;
-            s1 = c[i][0] &  3;
-            r2 = c[i][1] >> 2 ;
-            s2 = c[i][1] &  3;
-            std::cout << " " << holeCardsToChar(r1, s1) << holeCardsToChar(r2, s2) ;
-        }
-        std::cout << std::endl;
-        std::cout << "size of range: "<< size2 << std::endl;
-    }
-//    std::cout << results.equity[0] << " " << results.equity[1] <<" "<< results.equity[2] << std::endl;
-//    std::cout << results.equity[3] << " " << results.equity[4] << " " << results.equity[5] << std::endl;
-
-    std::array<double, 6> mydoubles{0, 0, 0, 0, 0, 0};
-    for(int i = 0; i < ranges.size(); i++) {
-        mydoubles[i] = i;
-        std::cout << mydoubles[i] << std::endl;
-    }
-
-    return  result;
+    return  mResults;
 
 }
 /*==============================================================================================*\
     FUNCTION :   playerCombos()
-    PURPOSE  :
-                 Returns a list of card combinations belonging to this range. Guarantees that there are no duplicates.
+    PURPOSE  :   Returns a list of card combinations belonging to this range. Guarantees that there are no duplicates.
                  Cards in each combo are ordered so that the bigger rank is always first. The whole vector is sorted in the
                  following order: 1) rank of first card 2) rank of second card 3) suit of first card 4) suit of second card
                  Create a Hand from a card. CardIdx is an integer between 0 and 51, so that CARD = 4 * RANK + SUIT, where
                  rank ranges from 0 (deuce) to 12 (ace) and suit is from 0 (spade) to 3 (diamond).
 
-    ARGUMENT :
-                 ranges : list of ranges for active players.
+    ARGUMENT :   ranges : list of ranges for active players.
                  board  :  board card
                  dead   :  dead card dealt alreay.
                  playerID: Specify player's ID whose combos will be returned. 0~5
                             if palyerID == 6, report combos of all players,
                             elseif          , report combos of specified player.
-    RETURN   :
-                 Return combos of specified player in list.
+    RETURN   :  Return combos of specified player in list.
 \*==============================================================================================*/
 std::vector<std::string> playerCombos(const std::vector<std::string>& ranges, std::string board, std::string dead, unsigned playerID)
 {
     std::vector<omp::CardRange> ranges2(ranges.begin(), ranges.end());
     std::vector<std::string> result;
 
-    // Set up card ranges.
+    /*-----------------------Set up card ranges----------------------- */
     unsigned mCombinedRangeCount, mPlayerCount;
     uint64_t mDeadCards, mBoardCards;
     std::vector<omp::CardRange> mOriginalHandRanges;
@@ -178,8 +127,6 @@ std::vector<std::string> playerCombos(const std::vector<std::string>& ranges, st
     mHandRanges = omp::EquityCalculator::removeInvalidCombos(ranges2, mDeadCards | mBoardCards);
     std::vector<omp::CombinedRange> combinedRanges = omp::CombinedRange::joinRanges(mHandRanges, MAX_COMBINED_RANGE_SIZE);
     mCombinedRangeCount = (unsigned)combinedRanges.size();
-
-
     /*-----------------------Combined Range ----------------------- */
     for (unsigned i = 0; i < mCombinedRangeCount; ++i) {
         mPlayerCount = combinedRanges[i].playerCount();
