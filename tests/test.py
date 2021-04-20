a17890da1c6717868d02506c1634fcb884e86d31
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2021-04-21 02:19:25 +0800

    1. Add pytest on montecarlo 2. test_playerCombos 6 tests.    test_montecarlo   4 tests.

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,10 @@ def _runner(ranges, cards_on_table, dead, expected_result):
     equity = mc.montecarlo(ranges, cards_on_table, dead)
     assert equity == pytest.approx(expected_result, abs=1e-3)
 
-
+def _runner1(ranges, cards_on_table, dead, playerID, expected_result):
+    combos = mc.playerCombos(ranges, cards_on_table, dead, playerID)
+    print(combos)
+    assert len(combos) == pytest.approx(expected_result, abs=1e-3)
 
 def test_montecarlo1():
     """Three-way test"""
@@ -39,6 +42,61 @@ def test_montecarlo4():
     expected_results = [0.504, 0.495]
     _runner(ranges, cards_on_table, dead, expected_results)
 
+def test_playerCombos1():
+    """random range test"""
+    ranges = ["random", "AhTs"]
+    cards_on_table = "7dJhKh4c"
+    dead = ""
+    playerID = 6
+    expected_results = 1035
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+def test_playerCombos2():
+    """Card removal test for preflop"""
+    ranges = ["random", "AhTs"]
+    cards_on_table = ""
+    dead = ""
+    playerID = 6
+    expected_results = 1225   # C(50,2)
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+def test_playerCombos3():
+    """Card removal test for flop"""
+    ranges = ["random", "AhTs"]
+    cards_on_table = "7dJhKh"
+    dead = ""
+    playerID = 6
+    expected_results = 1081   # C(47,2)
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+def test_playerCombos4():
+    """Customized range test for pair combos"""
+    ranges = ["66+"]
+    cards_on_table = ""
+    dead = ""
+    playerID = 6
+    expected_results = 54   # 9*6 = 54
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+def test_playerCombos5():
+    """Customized range test"""
+    ranges = ["66+,AJs+,KJs+,QJs,AJo+,KJo+,QJo"]
+    cards_on_table = ""
+    dead = ""
+    playerID = 6
+    expected_results = 150
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+def test_playerCombos6():
+    """Customized range test by dead card"""
+    ranges = ["66+,AJs+,KJs+,QJs,AJo+,KJo+,QJo"]
+    cards_on_table = ""
+    dead = "AhTd"
+    playerID = 6
+    expected_results = 132
+    _runner1(ranges, cards_on_table, dead, playerID, expected_results)
+
+# 66+,AJs+,KJs+,QJs,AJo+,KJo+,QJo
 
 #assert mc.playerCombos(ranges, cards_on_table, dead, 0) == ['3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3h3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3s', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h', '3d3h']
 
